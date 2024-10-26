525acb7ce3f47e3d1550435138a8e595bf8bf3ed
Winner! <105521740+Legomonster4444@users.noreply.github.com>
2024-10-25 20:49:50 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,3 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('openButton').addEventListener('click', openUrl);
+});
+
 function openUrl() {
     var url = document.getElementById('url').value;
     if (!url.startsWith('http://') && !url.startsWith('https://')) {
